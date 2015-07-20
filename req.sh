9cbb7bd600d7664276b6c3c90982265761fd7e33
SONALI SRIVASTAVA <srivastava.sonali1@gmail.com>
2015-07-20 19:17:04 +0530

    Update req.sh

--- req.sh
+++ req.sh
@@ -9,6 +9,6 @@ sudo apt-get install python-mysqldb
 #Set up a MySql database and user
 
 #Note, use the password you entered when installing MySql
-echo "CREATE DATABASE $DATABASE_NAME;"|mysql -u $Mysql_USER --password=$Mysql_pass
-echo "GRANT ALL ON $DATABASE_NAME.* TO '$USER_NAME'@'localhost' IDENTIFIED BY '$PASS';"|mysql -u $Mysql_USER --password=$Mysql_pass
-echo "EXIT"|mysql -u $Mysql_USER --password=$Mysql_pass
+echo "CREATE DATABASE $DATABASE_NAME;"|mysql -u $Mysql_USER --password=$Mysql_PASS
+echo "GRANT ALL ON $DATABASE_NAME.* TO '$USER_NAME'@'localhost' IDENTIFIED BY '$PASS';"|mysql -u $Mysql_USER --password=$Mysql_PASS
+echo "EXIT"|mysql -u $Mysql_USER --password=$Mysql_PASS
