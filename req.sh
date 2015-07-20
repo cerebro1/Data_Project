f13a54442fdf4477f2aa1ce6c1e387f3c893ecdf
SONALI SRIVASTAVA <srivastava.sonali1@gmail.com>
2015-07-20 18:27:48 +0530

    Update req.sh

--- req.sh
+++ req.sh
@@ -9,20 +9,6 @@ sudo apt-get install python-mysqldb
 #Set up a MySql database and user
 
 #Note, use the password you entered when installing MySql
-
-mysql -u root -p
-#Enter password: 
-#Welcome to the MySQL monitor.  Commands end with ; or \g.
-#Your MySQL connection id is 1
-#Server version: 5.0.51a-3ubuntu5.1 (Ubuntu)
-
-#Type 'help;' or '\h' for help. Type '\c' to clear the buffer.
-
-mysql> CREATE DATABASE DATABASE_NAME;
-Query OK, 1 row affected (0.01 sec)
-
-mysql> GRANT ALL ON @DATABASE_NAME.* TO '@USERNAME'@'@localhost' IDENTIFIED BY '@PASSWORD';
-Query OK, 0 rows affected (0.03 sec)
-
-mysql> quit
-Bye
+echo "CREATE DATABASE @DATABASE_NAME;"|mysql -u root --password=@PASSWORD
+echo "GRANT ALL ON @DATABASE_NAME.* TO '@USERNAME'@'localhost' IDENTIFIED BY '@PASSWORD';"|mysql -u root --password=@PASSWORD
+echo "EXIT"|
